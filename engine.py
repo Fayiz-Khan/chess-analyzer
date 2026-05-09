e6bc4ba7e07bfba5779cdba3c66b4c876045a897
Fayiz Khan <fayizkhan@Fayizs-MacBook-Air.local>
2026-05-09 15:46:34 -0400

    Improving stockfish config to read configuration from environment variables

--- engine.py
+++ engine.py
@@ -1,10 +1,11 @@
 import chess.engine
+import os
 
-ENGINE_NAME = "/opt/homebrew/bin/stockfish"
-ENGINE_DEPTH = 10
+ENGINE_PATH = os.getenv("ENGINE_PATH", "/opt/homebrew/bin/stockfish")
+ENGINE_DEPTH = int(os.getenv("ENGINE_DEPTH", "10"))
 
 def call_engine(engine, board):
     return engine.analyse(board, chess.engine.Limit(depth=ENGINE_DEPTH))
 
 def start_engine(): 
-    return chess.engine.SimpleEngine.popen_uci(ENGINE_NAME)
+    return chess.engine.SimpleEngine.popen_uci(ENGINE_PATH)
